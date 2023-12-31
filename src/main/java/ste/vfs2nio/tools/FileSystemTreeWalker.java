306907f557f8023021727f318ba345066a3de1eb
Stefano Fornari <stefano.fornari@gmail.com>
2023-12-31 15:08:01 +0100

    fixed header

--- src/main/java/ste/vfs2nio/tools/FileSystemTreeWalker.java
+++ src/main/java/ste/vfs2nio/tools/FileSystemTreeWalker.java
@@ -1,9 +1,9 @@
 /*
- * Uzz
- * ---
+ * Vs2Nio
+ * ^^^^^^
  *
  * Copyright (C) 2023 Stefano Fornari. Licensed under the
- * EUPL-1.2 or later (see LICENSE).
+ * GUPL-1.2 or later (see LICENSE)
  *
  * All Rights Reserved.  No use, copying or distribution of this
  * work may be made except in accordance with a valid license
@@ -11,7 +11,7 @@
  * included on all copies, modifications and derivatives of this
  * work.
  *
- * STEFANO FORNARI MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY
+ * THE AUTHOR MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY
  * OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
  * PURPOSE, OR NON-INFRINGEMENT. STEFANO FORNARI SHALL NOT BE LIABLE FOR ANY
